601ee88676070a4d633c77c699285c4873034841
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-09-10 12:23:16 +0200

    add image section

--- components/Section1.tsx
+++ components/Section1.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
+
 export default function Section1() {
   return (
     <section
       id="section1"
       className="relative h-[200vh] bg-transparent flex items-center justify-center z-10"
     >
-      <div className="w-full mx-10 bg-[#B3B0A4EB] rounded-t-full h-[200vh] flex items-start pt-80 justify-center">
+       <div className="w-full mx-10 bg-[#B3B0A4F5] rounded-t-full h-[200vh] flex flex-col items-start pt-80 justify-center">
         <div className="flex flex-row items-center justify-center gap-x-20 w-full mx-10">
             <div className="basis-1/2 flex items-center justify-center">
           <h2 className="text-3xl text-center max-w-2xs font-serif text-[#382B20]">
@@ -41,6 +43,23 @@ export default function Section1() {
           </div>
           </div>
         </div>
+          <div className="w-full px-20 mt-30 relative">
+            <Image
+              src="/section1image.jpg"
+              alt="Section 1 Image"
+              width={800}
+              height={600}
+              className="w-full h-auto rounded-[150px]"
+            />
+            {/* Gradient overlay for fade effect */}
+             <div className="absolute -bottom-[0.2px] left-0 right-0 h-1/2 bg-gradient-to-t from-[#B3B0A4F5] to-transparent rounded-b-[150px] mx-20"></div>
+            <div className="absolute bottom-10 left-42 text-blue-200 z-10">
+              <div className="text-3xl text-stone-600 font-serif font-bold">Brussels</div>
+              <div className="text-sm"><span className="font-bold">Location:</span> Brussels</div>
+              <div className="text-sm"><span className="font-bold">Modality:</span> Climate Futures</div>
+              <div className="text-sm"><span className="font-bold">People:</span> 70</div>
+            </div>
+          </div>
       </div>
     </section>
   );
